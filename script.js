2d67f5a821fbcc94ce08203fe12524d0632e07df
Nadia <nadiastrangis1990@gmail.com>
2024-05-09 22:01:51 -0300

    mejoras varias

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtener referencia al botón
-    var myButton = document.getElementById("myButton");
-
-    // Obtener referencia al mensaje oculto
-    var message = document.getElementById("message");
-
-    // Agregar evento de clic al botón
-    myButton.addEventListener("click", function() {
-        // Mostrar el mensaje oculto
-        message.style.display = "block";
-    });
-});
+function mostrarAlerta() {
+    alert("¡Has hecho clic en Leer más!");
+}
